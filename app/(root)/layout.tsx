f986c08022a6755c9bb4303e95faef336e814b20
Mustafa Isaev <mustafaisaev3@gmail.com>
2024-01-04 00:35:21 +0300

    Making User Box

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '@/components/shared/Navbar'
+import Sidebar from '@/components/shared/Sidebar'
 import { ChildProps } from '@/types'
 
 const RootLayout = ({ children }: ChildProps) => {
   return (
     <div>
         <Navbar />
+        <Sidebar />
         {children}
     </div>
   )
