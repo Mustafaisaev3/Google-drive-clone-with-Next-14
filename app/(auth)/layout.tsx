40442f2469cf5dbe17de00538bbefa29e5dbdfc1
Mustafa Isaev <mustafaisaev3@gmail.com>
2024-01-03 22:38:34 +0300

    Making Sidebar UI

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,9 +6,10 @@ import React from 'react'
 const AuthLayout = ({ children }: ChildProps) => {
   return (
     <div className='relative'>
+      <div className="w-screen h-screen absolute inset-0 z-40 bg-black/50" />
       <Navbar />
       <Sidebar />
-      <main className='w-full h-[90vh] flex items-center justify-center z-50 relative'>
+      <main className='w-full h-[90vh] flex items-center justify-center relative z-40'>
         {children}
       </main>
     </div>
--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,15 +1,64 @@
+'use client'
+
 import React from 'react'
+import Link from 'next/link';
+import { Clock5, Cloud, Loader, Plus, Star, Tablet, Trash } from "lucide-react";
+import Item from './Item';
 import { Button } from '../ui/button'
-import { Plus } from 'lucide-react'
+import { Progress } from '../ui/progress';
+
+const sidebarLinks = [
+    {
+      label: "My drive",
+      icon: Tablet,
+      path: "/",
+    },
+    {
+      label: "Starred",
+      icon: Star,
+      path: "/starred",
+    },
+    {
+      label: "Recent",
+      icon: Clock5,
+      path: "/recent",
+    },
+    {
+      label: "Trash",
+      icon: Trash,
+      path: "/trash",
+    },
+    {
+      label: "Storage",
+      icon: Cloud,
+      path: "/cloud",
+    },
+];
 
 const Sidebar = () => {
   return (
-    <div className="h-[90vh] w-60 fixed top-[10vh] left-0 z-30 bg-[#F6F9FC] dark:bg-[#1f1f1f]">
+    <div className="h-[90vh] w-60 fixed top-[10vh] left-0 z-30 bg-[#F6F9FC] dark:bg-[#1f1f1f] border-r">
       <div className='flex flex-col p-3'>
         <Button className='w-fit h-12 rounded-full px-6 cursor-pointer'>
           <Plus />
           <span>New</span>
         </Button>
+        <div className="flex flex-col space-y-6 mt-8">
+          {sidebarLinks.map((link) => (
+            <Link href={link.path} key={link.path}>
+              <Item icon={link.icon} label={link.label} path={link.path} />
+            </Link>
+          ))}
+
+          <div className="flex flex-col space-y-2 mx-4">
+            <Progress className='h-2' value={30} />
+            <span>150 MB of 1.5 GB used</span>
+            <Button className="rounded-full" variant={"outline"}>
+            Get more storage
+            </Button>
+          </div>
+        </div>
+
       </div>
     </div>
   )
